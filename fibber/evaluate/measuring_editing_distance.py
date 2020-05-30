20f29d5f6c4de75b2ef4948e58f938a94cf18a31
Lei Xu <leix@mit.edu>
2020-05-30 02:20:56 -0400

    measurements

---
+++ fibber/evaluate/measuring_editing_distance.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+class EditingDistance(object):
+  """This class measures the editing distance between two sentences."""
+
+  def __init__(self):
+    super(EditingDistance, self).__init__()
+
+  def _lcs(self, s1, s2):
+    f = np.zeros((len(s1) + 1, len(s2) + 1), dtype='int')
+    for i in range(len(s1)):
+      for j in range(len(s2)):
+        f[i + 1][j + 1] = max(f[i][j + 1], f[i + 1][j])
+        if s1[i] == s2[j]:
+          f[i + 1][j + 1] = max(f[i + 1][j + 1], f[i][j] + 1)
+    return f[len(s1)][len(s2)]
+
+
+  def __call__(self, s1, s2):
+    s1 = s1.split()
+    s2 = s2.split()
+    if len(s1) == 0 or len(s2) == 0:
+      return len(s1) + len(s2)
+    return len(s1) - self._lcs(s1, s2)
+
+
+if __name__ == "__main__":
+  measure = EditingDistance()
+
+  s1 = "aa bb cc ad cc da"
+  s2 = "aa bb cc aa cc da"
+  print(s1)
+  print(s2)
+  print(measure(s1, s2))
+
+
+  s1 = "aa bb cc ad cc da"
+  s2 = "aa bb aa cc da"
+  print(s1)
+  print(s2)
+  print(measure(s1, s2))
---
+++ fibber/evaluate/measuring_use_similarity.py
@@ -0,0 +1,52 @@
+import logging
+import os
+
+import tensorflow as tf
+import tensorflow_hub as hub
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+tf.get_logger().setLevel('ERROR')
+
+
+class USESimilarity(object):
+  def __init__(self):
+    super(USESimilarity, self).__init__()
+    logging.info("load universal sentence encoder")
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/3"
+    self.embed = hub.Module(module_url)
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    self.sess = tf.Session(config=config)
+    self.build_graph()
+    self.sess.run([tf.global_variables_initializer(), tf.tables_initializer()])
+
+  def build_graph(self):
+    self.sts_input1 = tf.placeholder(tf.string, shape=(None))
+    self.sts_input2 = tf.placeholder(tf.string, shape=(None))
+
+    sts_encode1 = tf.nn.l2_normalize(self.embed(self.sts_input1), axis=1)
+    sts_encode2 = tf.nn.l2_normalize(self.embed(self.sts_input2), axis=1)
+    self.cosine_similarities = tf.reduce_sum(
+        tf.multiply(sts_encode1, sts_encode2), axis=1)
+    self.sim_scores = self.cosine_similarities
+
+  def __call__(self, s1, s2):
+    scores = self.sess.run(
+        [self.sim_scores],
+        feed_dict={
+            self.sts_input1: [s1],
+            self.sts_input2: [s2],
+        })
+    return scores[0][0]
+
+
+if __name__ == "__main__":
+  measure = USESimilarity()
+  s1 = "a is a a a a"
+  s2 = "rose is a rose"
+  print(s1, s2, measure(s1, s2), sep="\n")
+
+  s1 = "Saturday is the last day in a week"
+  s2 = "Sunday is the last day in a week"
+  print(s1, s2, measure(s1, s2), sep="\n")
