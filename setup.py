21cbfa4ee8277e68ab4336df20f27352f2d1ca9e
Lei Xu <leix@mit.edu>
2021-01-20 18:40:02 -0500

    Bump version: 0.2.1 → 0.2.2.dev0

--- setup.py
+++ setup.py
@@ -100,6 +100,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/DAI-Lab/fibber',
-    version='0.2.1',
+    version='0.2.2.dev0',
     zip_safe=False,
 )
