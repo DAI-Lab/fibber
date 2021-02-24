e84b82ea1b0ea705fa31bf58eb8be60461825f0c
Lei Xu <leix@mit.edu>
2021-02-24 17:11:54 -0500

    add non-autoregressive sampling to the benchmark

--- fibber/benchmark/benchmark.py
+++ fibber/benchmark/benchmark.py
@@ -8,7 +8,8 @@ from fibber.datasets import builtin_datasets, get_dataset, subsample_dataset, ve
 from fibber.metrics.attack_aggregation_utils import add_sentence_level_adversarial_attack_metrics
 from fibber.metrics.metric_utils import MetricBundle
 from fibber.paraphrase_strategies import (
-    BertSamplingStrategy, IdentityStrategy, RandomStrategy, TextAttackStrategy)
+    BertSamplingStrategy, IdentityStrategy, NonAutoregressiveBertSamplingStrategy, RandomStrategy,
+    TextAttackStrategy)
 from fibber.paraphrase_strategies.strategy_base import StrategyBase
 from fibber.robust_tuning_strategy.default_tuning_strategy import (
     DefaultTuningStrategy, TuningStrategyBase)
@@ -20,7 +21,8 @@ built_in_paraphrase_strategies = {
     "RandomStrategy": RandomStrategy,
     "IdentityStrategy": IdentityStrategy,
     "TextAttackStrategy": TextAttackStrategy,
-    "BertSamplingStrategy": BertSamplingStrategy
+    "BertSamplingStrategy": BertSamplingStrategy,
+    "NonAutoregressiveBertSamplingStrategy": NonAutoregressiveBertSamplingStrategy
 }
 
 built_in_tuning_strategies = {
@@ -210,19 +212,8 @@ class Benchmark(object):
 def get_strategy(arg_dict, dataset_name, strategy_name, strategy_gpu_id,
                  output_dir, metric_bundle):
     """Take the strategy name and construct a strategy object."""
-    if strategy_name == "RandomStrategy":
-        return RandomStrategy(arg_dict, dataset_name, strategy_gpu_id, output_dir, metric_bundle)
-    if strategy_name == "IdentityStrategy":
-        return IdentityStrategy(
-            arg_dict, dataset_name, strategy_gpu_id, output_dir, metric_bundle)
-    if strategy_name == "TextAttackStrategy":
-        return TextAttackStrategy(
-            arg_dict, dataset_name, strategy_gpu_id, output_dir, metric_bundle)
-    if strategy_name == "BertSamplingStrategy":
-        return BertSamplingStrategy(
-            arg_dict, dataset_name, strategy_gpu_id, output_dir, metric_bundle)
-    else:
-        assert 0
+    return built_in_paraphrase_strategies[strategy_name](
+        arg_dict, dataset_name, strategy_gpu_id, output_dir, metric_bundle)
 
 
 def main():
